35935c0638137c3d97716e1c8b0c53c864a1ae16
Benoit <bntnowak62@gmail.com>
2025-05-06 08:06:16 +0200

    FIX/ Refactor bouttons contact

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -4,8 +4,8 @@
 
 <template>
 <section class="contact">
-    <a href="tel:0658992189" class="button" title="Téléphone">06.58.99.21.89</a>
-    <a href="mailto:bntnowak@outlook.com" title="E-mail" class="button">bntnowak@outlook.com</a>
+    <a href="tel:0658992189" class="button" title="Téléphone">Téléphoner</a>
+    <a href="mailto:bntnowak@outlook.com" title="E-mail" class="button">Envoyer un mail</a>
     <a href="https://plateforme.freelance.com/freelance/Benoit-531dd5eb-f9be-44b2-9ebe-38d6f64c6fbb" title="Plateforme freelance" target="_blank" class="button" >Mon profil Freelance.com</a>
 </section>
 </template>
