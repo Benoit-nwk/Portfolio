dfa8e79275d363691e08ad56ac51331d99036b36
Benoit <bntnowak62@gmail.com>
2025-05-06 08:21:34 +0200

    FIX/ Refactor APP.vue

--- src/App.vue
+++ src/App.vue
@@ -5,17 +5,7 @@ import { RouterView} from 'vue-router'
 <template>
       <router-view/>
 </template>
-<<<<<<< HEAD
-=======
 
-<style>
->>>>>>> 150032b9db116214f2588bac5b0303f96cbac001
+<style scoped>
 
-<style>
-.fade-enter-active, .fade-leave-active {
-  transition: opacity 0.5s ease;
-}
-.fade-enter-from, .fade-leave-to {
-  opacity: 0;
-}
 </style>
