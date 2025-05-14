b4c0d7e8f28bd7ec2a13ab001680b7386bd934d9
Benoit <bntnowak62@gmail.com>
2025-05-14 12:54:56 +0200

    FEAT/ ajout d'un bouton de téléchargement de cv

--- src/components/About.vue
+++ src/components/About.vue
@@ -12,7 +12,8 @@
         </article>
         <article>
             <h2>Mon parcours</h2>
-            <p>Après avoir obtenu mon diplôme en informatique, j'ai travaillé sur divers projets, allant de la création de sites vitrines à des applications web complexes intégrant des API. J'ai acquis une solide expérience dans le développement front-end et back-end. </p>
+            <p>Pendant le cursus de développeur web et web mobile, j'ai travaillé sur divers projets, allant de la création de sites vitrines à des applications web complexes intégrant des API. J'ai acquis une solide expérience dans le développement front-end et back-end. </p>
+            <a href="/public/documents/CV_Nowak_Benoit.pdf" title="télécharger le cv">Télécharger mon CV</a>
         </article>
         <article>
             <h2>Mes compétences</h2>
@@ -20,8 +21,8 @@
         </article>
         <article>
             <h2>Mes projets</h2>
-            <p>J'ai travaillé sur de nombreux projets, allant de la création de sites vitrines à des applications web complexes intégrant des API.</p>
-            <aside><a href="/projects" title="Mes projets">Voir mes projets</a></aside>
+            <p>J'ai réalisé différentes applications web telles qu'un site vitrine pour un salon de coiffure mais aussi une reproduction de l'application Yuka. Je suis en train de travailler sur un jeu type web app.</p>
+            <a href="/projects" title="Mes projets">Voir mes projets</a>
         </article>
             </section>
 </template>
@@ -36,17 +37,28 @@
 }
 
 article{
-    padding: 1rem;
+    padding: 0.5rem;
     border-radius: 8px;
     box-shadow: none;
     transition: all 0.3s ease-in-out;
     display: flex;
     flex-direction: column;
-    justify-content: space-between;
+    justify-content: space-evenly;
     box-shadow: 0 4px 8px var(--primary-color);
     >p{
         text-align: start;
-        line-height: 1.5;
+        margin: 0;
+        line-height: 1.9rem;
+    }
+    >a{
+        text-decoration: none;
+        color: var(--primary-color);
+        font-weight: bold;
+        margin-top: 1rem;
+        padding: 0.5rem 1rem;
+        border-radius: 8px;
+        background-color: var(--secondary-color);
+        transition: all 0.3s ease-in-out;
     }
 }
 
