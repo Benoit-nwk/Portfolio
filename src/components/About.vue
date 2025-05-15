500e05b91adc03c00ff0a30a2822814f4a617a57
Benoit <bntnowak62@gmail.com>
2025-05-15 14:22:09 +0200

    FIX/ Ajout responsive page 'A propos'

--- src/components/About.vue
+++ src/components/About.vue
@@ -8,7 +8,7 @@ const openCV=()=> {
     <h1>A propos</h1>
     <section class="bento">
         <article>
-            <img src="/icons/profile_image.png" alt="profile_image" class="image_bento">
+            <img src="/icons/profile_image.png" alt="profile_image" class="image_bento image1">
             <aside>
             <h2>Qui suis-je?</h2>
             <p>Je suis un développeur web passionné par la création de sites et d'applications web. J'aime relever des défis techniques et trouver des solutions innovantes pour répondre aux besoins des clients.</p>   
@@ -19,10 +19,10 @@ const openCV=()=> {
             <h2>Mon expérience</h2>
             <p>J'ai travaillé sur divers projets, allant de la création de sites vitrines à des applications web complexes. J'ai acquis une solide expérience dans le développement front-end et back-end.</p>
             </aside>
-            <img src="/icons/experience_image.png" alt="exp_image" class="image_bento">
+            <img src="/icons/experience_image.png" alt="exp_image" class="image_bento image2">
         </article>
         <article>
-            <img src="/icons/skills_image.png" alt="skills_image" class="image_bento">
+            <img src="/icons/skills_image.png" alt="skills_image" class="image_bento image3">
             <aside>
             <h2>Mes compétences</h2>
             <p>Je maîtrise plusieurs langages de programmation et frameworks, notamment HTML, CSS, JavaScript, Vue.js, PHP et bien d'autres. Je suis toujours en quête d'apprentissage pour rester à jour avec les dernières technologies.</p>
@@ -33,37 +33,5 @@ const openCV=()=> {
 </template>
 
 <style scoped>
-.image_bento {
-    width: 200px;
-    height: 200px;
-}
 
-.bento{
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    flex-direction: column;
-    gap: 2rem;
-    margin-bottom: 2rem;
-}
-
-article {
-    width: 50%;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    flex-direction: row;
-    text-align: start;
-    border: 0.2rem solid var(--primary-color);
-    border-radius: 1rem;
-    padding : 2rem;
-}
-
-aside{
-    width: 50%;
-    padding: 2rem;
-    >p{
-        text-align: start;
-    }
-}
 </style>
